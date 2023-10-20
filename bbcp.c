4164f9c3f23de972095ff81f05fd667ebcd8a07d
kurth4cker <kurth4cker@tutanota.com>
2023-10-20 12:47:38 +0300

    cp fonksiyonu eklendi
    artık dosyalar cp fonksiyonunda açılacak. şimdilik dir desteği
    kaldırıldı. sonra eklenebilir

--- bbcp.c
+++ bbcp.c
@@ -17,18 +17,35 @@ bbcopy(int fd1, int fd2)
 	char buf[BUFSIZ];
 	ssize_t n;
 
-	for (;;) {
-		n = read(fd1, buf, sizeof(buf));
-		if (n <= 0)
-			break;
-
+	while ((n = read(fd1, buf, sizeof(buf))) > 0)
 		if (write(fd2, buf, n) == -1)
 			return -1;
-	}
 
 	return n;
 }
 
+static int
+cp(const char *s1, const char *s2)
+{
+	int fd1, fd2;
+	int status;
+	mode_t perm;
+
+	perm = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+
+	if ((fd1 = open(s1, O_RDONLY)) == -1)
+		err(EXIT_FAILURE, "can't read %s", s1);
+	if ((fd2 = creat(s2, perm)) == -1)
+		err(EXIT_FAILURE, "can't create %s", s2);
+
+	status = bbcopy(fd1, fd2);
+
+	close(fd2);
+	close(fd1);
+	return status;
+}
+
+/*
 static int
 dest_open(char *dest, char *src)
 {
@@ -57,12 +74,13 @@ just_open:
 	return open(file, O_WRONLY|O_CREAT|O_TRUNC,
 		S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
 }
+*/
 
 static void
 usage(int code)
 {
 	fprintf(stderr, "usage: %s file newfile\n", argv0);
-	fprintf(stderr, "usage: %s file dir\n", argv0);
+//	fprintf(stderr, "usage: %s file dir\n", argv0);
 	fprintf(stderr, "\n");
 	fprintf(stderr, "see also bbcp(1)\n");
 
@@ -72,7 +90,6 @@ usage(int code)
 int
 main(int argc, char **argv)
 {
-	int fd1, fd2;
 	int ch;
 
 	argv0 = argv[0];
@@ -90,19 +107,8 @@ main(int argc, char **argv)
 	if (argc < 3)
 		usage(EXIT_FAILURE);
 
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-		err(EXIT_FAILURE, "open %s for reading", argv[1]);
-
-	fd2 = dest_open(argv[2], argv[1]);
-	if (fd2 == -1)
-		err(EXIT_FAILURE, "cannot copy to %s", argv[2]);
-
-	if (bbcopy(fd1, fd2) == -1)
-		err(EXIT_FAILURE, "cannot copy %s to %s", argv[1], argv[2]);
-
-	close(fd2);
-	close(fd1);
+	if (cp(argv[1], argv[2]) == -1)
+		err(EXIT_FAILURE, "can't copy %s to %s", argv[1], argv[2]);
 
 	return 0;
 }
