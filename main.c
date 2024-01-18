254a9987e7c1bce33def2d1362db1b7eb9ddce38
Kurth4cker <kurth4cker@gmail.com>
2024-01-18 08:35:51 +0300

    Add license: GPL-3.0-or-later

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
-/* See LICENSE file for copyright and license details. */
+/* Copyright 2024 kurth4cker <kurth4cker@gmail.com>
+ * Licensed under the terms of GPL-3.0-or-later.
+ * See file COPYING for copyright and license details.
+ */
 
 #include <stdio.h>
 
